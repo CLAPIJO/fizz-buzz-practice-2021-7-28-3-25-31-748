d5cee948fcb64b2a0c136597fc833a9c6a8a6b99
Jose Marlouie Clapis <jose.marlouie.clapis@oocl.com>
2022-09-01 14:36:19 +0800

    test: count off should return FizzBuzz when given multiple of 3 and 5

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -48,4 +48,15 @@ public class FizzBuzzTest {
         //then
         assertEquals(expected, actual);
     }
+
+    @Test
+    void should_return_FizzBuzz_when_count_off_given_number_15() {
+        //given
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        String expected = "FizzBuzz";
+        //when
+        String actual = fizzBuzz.countOff(15);
+        //then
+        assertEquals(expected, actual);
+    }
 }
