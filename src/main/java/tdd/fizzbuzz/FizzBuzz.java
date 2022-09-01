fbfeaa45064056de2947f4569bd5cb11e81ecfa5
Jose Marlouie Clapis <jose.marlouie.clapis@oocl.com>
2022-09-01 14:34:31 +0800

    feat: implement the count off if number is multiple of 7

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -3,12 +3,16 @@ package tdd.fizzbuzz;
 public class FizzBuzz {
     String FIZZ = "Fizz";
     String BUZZ = "Buzz";
+    String WHIZZ = "Whizz";
     public  String countOff(int order){
         if (order % 3 == 0){
             return FIZZ;
         }
         if (order % 5 == 0){
             return BUZZ;
+        }
+        if (order % 7 == 0){
+            return WHIZZ;
         }
         return String.valueOf(order);
     }
