f5c81b5ef04c0dde6b8787a96c977c466944b559
Jose Marlouie Clapis <jose.marlouie.clapis@oocl.com>
2022-09-01 14:24:12 +0800

    feat: implement the count off if number is multiple of 3

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -1,7 +1,11 @@
 package tdd.fizzbuzz;
 
 public class FizzBuzz {
+    String FIZZ = "Fizz";
     public  String countOff(int order){
+        if (order % 3 == 0){
+            return FIZZ;
+        }
         return String.valueOf(order);
     }
 }
