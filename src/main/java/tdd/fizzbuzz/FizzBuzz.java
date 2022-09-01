3abd9198876b7a120239f243e64babe76dcdeb88
Jose Marlouie Clapis <jose.marlouie.clapis@oocl.com>
2022-09-01 14:32:02 +0800

    feat: implement the count off if number is multiple of 5

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -2,9 +2,13 @@ package tdd.fizzbuzz;
 
 public class FizzBuzz {
     String FIZZ = "Fizz";
+    String BUZZ = "Buzz";
     public  String countOff(int order){
         if (order % 3 == 0){
             return FIZZ;
+        }
+        if (order % 5 == 0){
+            return BUZZ;
         }
         return String.valueOf(order);
     }
